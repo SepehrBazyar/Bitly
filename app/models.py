d62eda92b3853e20e2f1773bf76c285b2d784ac4
Sepehr Bazyar <sepehr.bazyar@karnameh.ir>
2026-01-05 11:58:14 +0330

    style: use timezone utc instead deprecated utc now

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import UTC, datetime
 
 from sqlmodel import (
     Field,
@@ -11,7 +11,9 @@ class URL(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     original_url: str
     short_code: str = Field(index=True, unique=True)
-    created_at: datetime = Field(default_factory=datetime.utcnow)
+    created_at: datetime = Field(
+        default_factory=lambda: datetime.now(UTC),
+    )
     visits: list["Visit"] = Relationship(back_populates="url")
 
 
@@ -19,5 +21,7 @@ class Visit(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     url_id: int = Field(foreign_key="url.id")
     ip: str
-    timestamp: datetime = Field(default_factory=datetime.utcnow)
+    timestamp: datetime = Field(
+        default_factory=lambda: datetime.now(UTC),
+    )
     url: URL | None = Relationship(back_populates="visits")
