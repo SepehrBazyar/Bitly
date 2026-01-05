8bba745341a38d547194b969005d25655fd2a0dc
Sepehr Bazyar <sepehr.bazyar@karnameh.ir>
2026-01-05 15:38:46 +0330

    feat: index home template html

--- app/main.py
+++ app/main.py
@@ -1,10 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 from app.middleware import logging_middleware
 from app.routers import urls
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount(
+    "/static",
+    StaticFiles(directory="static"),
+    name="static",
+)
 
 
 app.middleware("http")(logging_middleware)
 app.include_router(urls.router)
+
+
+@app.get("/")
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
